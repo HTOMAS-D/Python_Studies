4b73a65b0f90ee3132fd58602076a082febfd683
Ricky <henryats@gmail.com>
2023-02-20 14:56:42 +0000

    some more stuff on for loops in python

--- basic_for_loop.py
+++ basic_for_loop.py
@@ -1,9 +1,20 @@
-n = 10
+# n = 10
 
-for row in range(n):
-	for col in range (n):
-		if row+col < n:
-			print('', end=" ")
-		else:
-			print("#", end = ' ')
-	print('#')
+# for row in range(n):
+# 	for col in range (n):
+# 		if row+col < n:
+# 			print('', end=" ")
+# 		else:
+# 			print("#", end = ' ')
+# 	print('#')
+
+
+###2 WAYS OF WRITING LOOPS AND RUNNING PROGRAMS -- one by using mains and functions and another by just writing code.
+
+def ricky(n):
+	for row in range(n):
+		print((''*(n + row))+
+		('#'*row) + '##')
+
+if __name__ == '__main__':
+	ricky(10)
